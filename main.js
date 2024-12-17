bc21b9ece0aa6f0a34b85e8fa6df5d97ac3ffa70
sanayaseen3 <sanayaseen757@gmail.com>
2024-12-17 13:26:42 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+//swiper
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    
+  });
+  let menu = document.querySelector(".menu-icon");
+  let navbar = document.querySelector(".navbar");
+
+  menu.onclick = () => {
+    menu.classList.toggle("move");
+    navbar.classList.toggle("open-menu");
+  };
+  //close menu on scroll
+  window.onscroll = () => {
+    menu.classList.remove("move");
+    navbar.classList.remove("open-menu");
+  };
+  //scroll reveal animation
+  const animate = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: '2500',
+    delay: '400',  
+  });
+
+  animate.reveal(".navbar");
+  animate.reveal(".home-text",{origin: "left"});
+  animate.reveal(".home-img",{origin: "bottom"});
+  animate.reveal(".ser-box",{interval:100});
+  animate.reveal(".product-box",{interval:100});
+  animate.reveal(".team-box",{interval:100});
+  animate.reveal(".book-data",{interval:100});
+
+
